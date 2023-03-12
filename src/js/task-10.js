4768d23ffaa231f5b052e50b53ee035b4e80c973
DimaSvirin <dimasvirin@gmail.com>
2023-03-12 21:35:03 +0200

    Task 10 start

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,39 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls > input')
+const createBtn = document.querySelector('button[data-create]');
+
+function createBoxes(amount) {
+  let container = [];
+  const box = document.createElement('div')
+  container.push(box)
+  return container
+
+}
+createBtn.addEventListener('click', () => {
+  console.log("lalal")
+  const result = createBoxes(1)
+  console.log(result)
+})
